4b8685b776417b8a4075d400cd3cec0fe41c8cab
Deevela Ravi Teja <deevelaraviteja@gmail.com>
2026-01-09 11:58:48 +0530

    Update main.tf

--- Terraform/main.tf
+++ Terraform/main.tf
@@ -1,4 +1,5 @@
 
+# vpc
 resource "aws_vpc" "cloudzenia_vpc" {
   cidr_block           = "10.0.0.0/16"
   enable_dns_support   = true
@@ -9,6 +10,7 @@ resource "aws_vpc" "cloudzenia_vpc" {
   }
 }
 
+# internet gateway
 resource "aws_internet_gateway" "cloudzenia_igw" {
   vpc_id = aws_vpc.cloudzenia_vpc.id
 
@@ -17,6 +19,7 @@ resource "aws_internet_gateway" "cloudzenia_igw" {
   }
 }
 
+# subnets
 resource "aws_subnet" "cloudzenia_public_1" {
   vpc_id                  = aws_vpc.cloudzenia_vpc.id
   cidr_block              = "10.0.1.0/24"
@@ -59,6 +62,7 @@ resource "aws_subnet" "cloudzenia_private_2" {
   }
 }
 
+# nat gateway
 resource "aws_eip" "cloudzenia_nat_eip" {
   domain = "vpc"
 
@@ -78,6 +82,7 @@ resource "aws_nat_gateway" "cloudzenia_nat" {
   }
 }
 
+# route tables
 resource "aws_route_table" "cloudzenia_public_rt" {
   vpc_id = aws_vpc.cloudzenia_vpc.id
 
@@ -87,7 +92,7 @@ resource "aws_route_table" "cloudzenia_public_rt" {
   }
 
   tags = {
-    Name = "cloudzenia-public-route-table"
+    Name = "cloudzenia-public-rt"
   }
 }
 
@@ -100,30 +105,32 @@ resource "aws_route_table" "cloudzenia_private_rt" {
   }
 
   tags = {
-    Name = "cloudzenia-private-route-table"
+    Name = "cloudzenia-private-rt"
   }
 }
 
-resource "aws_route_table_association" "cloudzenia_public_1_assoc" {
+# route table associations
+resource "aws_route_table_association" "public_1_assoc" {
   subnet_id      = aws_subnet.cloudzenia_public_1.id
   route_table_id = aws_route_table.cloudzenia_public_rt.id
 }
 
-resource "aws_route_table_association" "cloudzenia_public_2_assoc" {
+resource "aws_route_table_association" "public_2_assoc" {
   subnet_id      = aws_subnet.cloudzenia_public_2.id
   route_table_id = aws_route_table.cloudzenia_public_rt.id
 }
 
-resource "aws_route_table_association" "cloudzenia_private_1_assoc" {
+resource "aws_route_table_association" "private_1_assoc" {
   subnet_id      = aws_subnet.cloudzenia_private_1.id
   route_table_id = aws_route_table.cloudzenia_private_rt.id
 }
 
-resource "aws_route_table_association" "cloudzenia_private_2_assoc" {
+resource "aws_route_table_association" "private_2_assoc" {
   subnet_id      = aws_subnet.cloudzenia_private_2.id
   route_table_id = aws_route_table.cloudzenia_private_rt.id
 }
 
+# security group
 resource "aws_security_group" "cloudzenia_sg" {
   name   = "cloudzenia-sg"
   vpc_id = aws_vpc.cloudzenia_vpc.id
@@ -142,6 +149,13 @@ resource "aws_security_group" "cloudzenia_sg" {
     cidr_blocks = ["0.0.0.0/0"]
   }
 
+  ingress {
+    from_port   = 8080
+    to_port     = 8080
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
   egress {
     from_port   = 0
     to_port     = 0
@@ -154,11 +168,12 @@ resource "aws_security_group" "cloudzenia_sg" {
   }
 }
 
-resource "aws_instance" "cloudzenia_public_ec2_1" {
-  ami                         = "ami-00f46ccd1cbfb363e"
+# ec2 instances
+resource "aws_instance" "public_ec2_1" {
+  ami                         = "ami-06f1fc9ae5ae7f31e"
   instance_type               = "t2.micro"
   subnet_id                   = aws_subnet.cloudzenia_public_1.id
-  key_name                    = "Ravi-Teja-Key"
+  key_name                    = "Cloud-Zenia"
   vpc_security_group_ids      = [aws_security_group.cloudzenia_sg.id]
   associate_public_ip_address = true
 
@@ -167,11 +182,11 @@ resource "aws_instance" "cloudzenia_public_ec2_1" {
   }
 }
 
-resource "aws_instance" "cloudzenia_public_ec2_2" {
-  ami                         = "ami-00f46ccd1cbfb363e"
+resource "aws_instance" "public_ec2_2" {
+  ami                         = "ami-06f1fc9ae5ae7f31e"
   instance_type               = "t2.micro"
   subnet_id                   = aws_subnet.cloudzenia_public_2.id
-  key_name                    = "Ravi-Teja-Key"
+  key_name                    = "Cloud-Zenia"
   vpc_security_group_ids      = [aws_security_group.cloudzenia_sg.id]
   associate_public_ip_address = true
 
@@ -180,11 +195,11 @@ resource "aws_instance" "cloudzenia_public_ec2_2" {
   }
 }
 
-resource "aws_instance" "cloudzenia_private_ec2_1" {
-  ami                    = "ami-00f46ccd1cbfb363e"
+resource "aws_instance" "private_ec2_1" {
+  ami                    = "ami-06f1fc9ae5ae7f31e"
   instance_type          = "t2.micro"
   subnet_id              = aws_subnet.cloudzenia_private_1.id
-  key_name               = "Ravi-Teja-Key"
+  key_name               = "Cloud-Zenia"
   vpc_security_group_ids = [aws_security_group.cloudzenia_sg.id]
 
   tags = {
@@ -192,14 +207,99 @@ resource "aws_instance" "cloudzenia_private_ec2_1" {
   }
 }
 
-resource "aws_instance" "cloudzenia_private_ec2_2" {
-  ami                    = "ami-00f46ccd1cbfb363e"
+resource "aws_instance" "private_ec2_2" {
+  ami                    = "ami-06f1fc9ae5ae7f31e"
   instance_type          = "t2.micro"
   subnet_id              = aws_subnet.cloudzenia_private_2.id
-  key_name               = "Ravi-Teja-Key"
+  key_name               = "Cloud-Zenia"
   vpc_security_group_ids = [aws_security_group.cloudzenia_sg.id]
 
   tags = {
     Name = "cloudzenia-private-ec2-2"
+  }
+}
+
+# application load balancer
+resource "aws_lb" "cloudzenia_alb" {
+  name               = "ALB-CZ"
+  load_balancer_type = "application"
+  internal           = false
+  security_groups    = [aws_security_group.cloudzenia_sg.id]
+
+  subnets = [
+    aws_subnet.cloudzenia_public_1.id,
+    aws_subnet.cloudzenia_public_2.id
+  ]
+
+  tags = {
+    Name = "ALB-CZ"
+  }
+}
+
+# target groups
+resource "aws_lb_target_group" "tg_1" {
+  name        = "TG-1"
+  port        = 80
+  protocol    = "HTTP"
+  vpc_id      = aws_vpc.cloudzenia_vpc.id
+  target_type = "instance"
+
+  health_check {
+    path = "/"
+  }
+}
+
+resource "aws_lb_target_group" "tg_2" {
+  name        = "TG-2"
+  port        = 8080
+  protocol    = "HTTP"
+  vpc_id      = aws_vpc.cloudzenia_vpc.id
+  target_type = "instance"
+
+  health_check {
+    path = "/"
+    port = "8080"
+  }
+}
+
+# target group attachments
+resource "aws_lb_target_group_attachment" "tg1_attach" {
+  target_group_arn = aws_lb_target_group.tg_1.arn
+  target_id        = aws_instance.private_ec2_1.id
+  port             = 80
+}
+
+resource "aws_lb_target_group_attachment" "tg2_attach" {
+  target_group_arn = aws_lb_target_group.tg_2.arn
+  target_id        = aws_instance.private_ec2_2.id
+  port             = 8080
+}
+
+# alb listener
+resource "aws_lb_listener" "alb_listener" {
+  load_balancer_arn = aws_lb.cloudzenia_alb.arn
+  port              = 80
+  protocol          = "HTTP"
+
+  default_action {
+    type             = "forward"
+    target_group_arn = aws_lb_target_group.tg_1.arn
+  }
+}
+
+# listener rule
+resource "aws_lb_listener_rule" "tg2_rule" {
+  listener_arn = aws_lb_listener.alb_listener.arn
+  priority     = 100
+
+  action {
+    type             = "forward"
+    target_group_arn = aws_lb_target_group.tg_2.arn
+  }
+
+  condition {
+    path_pattern {
+      values = ["/app2*"]
+    }
   }
 }
